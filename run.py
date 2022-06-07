146084c5146ece359cabb722eb3c996e1ab5edd6
Ryan O'Neill <ryanoneill416@gmail.com>
2022-06-07 19:59:14 +0000

    Add homepage function to display application heading

--- run.py
+++ run.py
@@ -7,6 +7,7 @@ operations of a car dealership
 
 import gspread
 from google.oauth2.service_account import Credentials
+from pyfiglet import Figlet
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -21,3 +22,16 @@ SHEET = GSPREAD_CLIENT.open('automated_autos')
 
 inventory = SHEET.worksheet('inventory')
 sales = SHEET.worksheet('sales')
+
+
+def display_homepage():
+    """
+    Displays the homepage of the application
+    Provides the user with a header
+    Declares the functionality of the application
+    Brief accreditation
+    """
+    fig_font = Figlet(font='slant')
+    print(fig_font.renderText('Automated\nAuto Dealer\n'))
+    print('------ Inventory Management Tool For Auto Traders ------')
+    print('--------------- Created By RyanONeill416 ---------------')
